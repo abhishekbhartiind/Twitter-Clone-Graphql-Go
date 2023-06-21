6ac1d5993958c6152cec2864bd39ded37ab54c7e
Last-dreamer <asim0000.ak@gmail.com>
2023-06-21 20:30:00 +0400

    adding user repo

--- postgres/user_repo.go
+++ postgres/user_repo.go
@@ -6,7 +6,8 @@ import (
 	"twitter"
 
 	"github.com/georgysavva/scany/pgxscan"
-	"github.com/georgysavva/scany/v2/pgxscan"
+	// "github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/jackc/pgx/v4"
 )
 
 type UserRepo struct {
@@ -14,8 +15,36 @@ type UserRepo struct {
 }
 
 func (ur *UserRepo) CreateUser(c context.Context, user twitter.User) (twitter.User, error) {
-	panic("implement me ")
+
+	tx, err := ur.DB.Pool.Begin(c)
+	if err != nil {
+		return twitter.User{}, fmt.Errorf("err while begin transaction %v", err)
+	}
+	defer tx.Rollback(c)
+
+	user, err = createUser(c, tx, user)
+	if err != nil {
+		return twitter.User{}, err
+	}
+
+	if err := tx.Commit(c); err != nil {
+		return twitter.User{}, fmt.Errorf("error while commiting %v", err)
+	}
+
+	return user, nil
 }
+
+func createUser(c context.Context, tx pgx.Tx, user twitter.User) (twitter.User, error) {
+	query := `INSERT INTO users (username, email,password) VALUES ($1, $2, $3) RETURNING *;`
+
+	u := twitter.User{}
+
+	if err := pgxscan.Get(c, tx, &u, query, user.Username, user.Email, user.Password); err != nil {
+		return twitter.User{}, fmt.Errorf("error while inserting %v", err)
+	}
+	return u, nil
+}
+
 func (ur *UserRepo) GetByUsername(ctx context.Context, username string) (twitter.User, error) {
 	query := `SELECT * FROM users WHERE id = $1 LIMIT 1`
 	u := twitter.User{}
